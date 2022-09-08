342b182ca9052845cf29ec01aa78cfcc11751119
Igor Lysak <lysak.igor@gmail.com>
2022-09-08 17:26:10 +0300

    Update JupyterLab component #16

--- kubeflow-jupyter/undeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-kubectl="kubectl -n $NAMESPACE --context=$HUB_DOMAIN_NAME"
-
-files download-tar "$URL" "jupyter-web-app/kustomize" --tar-subpath "jupyter/jupyter-web-app"
-files download-tar "$URL" "notebook-controller/kustomize" --tar-subpath "jupyter/notebook-controller"
-
-$kubectl delete --ignore-not-found=true -k "notebook-controller"
-$kubectl delete --ignore-not-found=true -k "jupyter-web-app"
