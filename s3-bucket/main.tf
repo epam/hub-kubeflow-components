09e46fecf36185bbb959b0859db854026b6a8f2c
Neli Hargeliya <nelly.hargeliya@gmail.com>
2023-08-24 15:13:55 +0300

    S3-bucket component added to the stack to deploy the AWS bucket (#47)
    Co-authored-by: Neli_Hargeliya <neli_hargeliya@epam>

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -2,37 +2,25 @@ variable "name" {
   type = string
 }
 
-variable "service_account_name" {
-  type    = string
-  default = ""
+locals {
+  s3_endpoint = "s3.${module.s3_bucket.s3_bucket_region}.amazonaws.com"
 }
 
 variable "acl" {
-  type = string
+  type        = string
   description = "S3 bucket ACL"
-}
-
-variable "bucket_region" {
-  type = string
-  description = "s3 bucket region"
 }
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.name
-  acl    = var.acl
-
+  bucket                   = var.name
+  acl                      = var.acl
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
 
-  website = {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
   versioning = {
-    enabled = false
+    enabled = true
   }
 
   tags = {
